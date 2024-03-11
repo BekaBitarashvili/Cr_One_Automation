cb36766554828aa4172f67fbbf9f5626c8d314b5
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-03-11 10:24:11 +0400

    Modified

--- automation_parts/auth.py
+++ automation_parts/auth.py
@@ -261,3 +261,16 @@ class TestWebsite(unittest.TestCase):
         # )
         # otp_button.click()
         time.sleep(3)
+
+
+# import urllib.request
+# import urllib.request
+# import urllib.error
+#
+# try:
+#     with urllib.request.urlopen('http://10.117.27.38:8090/') as f:
+#         print(f.read())
+#         print(f.status)
+#         print(f.getheader("content-length"))
+# except urllib.error.URLError as e:
+#     print(e.reason)
