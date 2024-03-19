9b1e54f2650d1ad3bec0023db43c5e79e58842ed
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-03-19 10:29:35 +0400

    500 Error Page

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return '500 Internal Server Error'
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
