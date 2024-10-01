482e501134f7e0f381cad3afd08f20848a2253c5
Beka Bitarashvili <bekabitarashvili@gmail.com>
2024-10-01 12:53:30 +0400

    OnlineLoan Details

--- automation_parts/onlineloan.py
+++ automation_parts/onlineloan.py
@@ -4,6 +4,7 @@ import time
 import random
 
 import pyautogui
+from PIL.EpsImagePlugin import field
 from selenium.webdriver.support.ui import Select
 from pywinauto.keyboard import send_keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -81,18 +82,88 @@ class TestWebsite(unittest.TestCase):
         time.sleep(3)
 
     def test_04_consent_to_data(self):
-        time.sleep(3)
+        time.sleep(1)
         x = 15
         while x > 0:
             send_keys('{DOWN}')
             x -= 1
-        time.sleep(2)
+        time.sleep(1)
         self.driver.find_element(By.ID, "isChecked").click()
-        time.sleep(2)
+        time.sleep(1)
         self.driver.find_element(By.ID, "isChecked2").click()
-        time.sleep(2)
+        time.sleep(1)
         self.driver.find_element(By.ID, "isChecked3").click()
 
     def test_05_agree_button(self):
         self.driver.find_element(By.ID, "confirm-btn").click()
+        time.sleep(2)
+
+    def test_06_income_types(self):
+        sector = self.driver.find_element(By.ID, "sector")
+        sector.click()
+        sector.send_keys(Keys.ARROW_DOWN)
+        sector.send_keys(Keys.ENTER)
+        time.sleep(1)
+        income_field = self.driver.find_element(By.ID, "field")
+        income_field.click()
+        income_field.send_keys(Keys.ARROW_DOWN)
+        income_field.send_keys(Keys.ENTER)
+        time.sleep(1)
+        subfield = self.driver.find_element(By.ID, "subField")
+        subfield.click()
+        subfield.send_keys(Keys.ARROW_DOWN)
+        subfield.send_keys(Keys.ENTER)
+        time.sleep(1)
+        amount = self.driver.find_element(By.ID, "amount")
+        amount.send_keys(random.randint(1000, 9999))
+        time.sleep(1)
+        income_currency = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section/div/form/div[1]/div["
+                                                             "1]/div[5]/div/div/div/div/div/div/div/span[1]/input")
+        income_currency.click()
+        income_currency.send_keys(Keys.ARROW_DOWN)
+        income_currency.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # დასამატებელია Attachment-ების ატვირთვა
+        # დასამატებელია წაშლა
+        # დასამატებელია ღილაკი "დაამატე შემოსავალი"
+        # დასამატებელია გზავნილის შემოსავლის ვარიანტი
+
+        submit_butt = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section/div/form/div["
+                                                         "3]/div/div/div/div/button")
+        submit_butt.click()
+
+    def test_07_loan_details(self):
+        loan_amount = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section/div/form/div["
+                                                         "3]/div/div/div/div/div/input")
+        loan_amount.send_keys(random.randint(9999, 99999))
+        time.sleep(1)
+        loan_duration = self.driver.find_element(By.ID, "duration")
+        loan_duration.click()
+        loan_duration.send_keys(random.randint(12, 24))
+        loan_duration.send_keys(Keys.ENTER)
+        time.sleep(1)
+        payment_date = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section/div/form/div[2]/div["
+                                                          "2]/div/div/div/div/div/div/div/span[1]/input")
+        payment_date.click()
+        payment_date.send_keys(Keys.ARROW_DOWN)
+        payment_date.send_keys(Keys.ENTER)
+        time.sleep(1)
+        payment_amount = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section/div/form/div["
+                                                            "3]/div/div/div/div/div/input")
+        payment_amount.send_keys(random.randint(1000, 9999))
+        time.sleep(1)
+        branch = self.driver.find_element(By.ID, "branch")
+        branch.click()
+        branch.send_keys(Keys.ARROW_DOWN)
+        branch.send_keys(Keys.ENTER)
+        time.sleep(1)
+        purposeoftheloan = self.driver.find_element(By.ID, "purposeOfTheLoan")
+        purposeoftheloan.click()
+        purposeoftheloan.send_keys(Keys.ARROW_DOWN)
+        purposeoftheloan.send_keys(Keys.ENTER)
+        time.sleep(1)
+        send_application = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/section/div/form/div["
+                                                              "6]/div/div/div/div/button")
+        send_application.click()
         time.sleep(2)
